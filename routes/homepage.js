3b7f9d1b2dc1007b50097f3dff67a1a29d767bf5
Divyang <divyang@git.com>
2023-02-28 20:29:50 -0500

    fixed homepage route

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-    try {
-        return res.status(200).render("homePage", {title:"Home Page"});
-    } catch (error) {
-        res.status(500).send(error);
-    }
+	try {
+		return res.status(200).render("homepage", { title: "Home Page" });
+	} catch (error) {
+		res.status(500).send(error);
+	}
 });
 
 module.exports = router;
