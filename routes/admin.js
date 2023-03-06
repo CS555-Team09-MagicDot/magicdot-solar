5cbf195e9ddb1aa8876db3ce53e77f70d5c162e1
Hem Patel <65719718+Hem-1112@users.noreply.github.com>
2023-03-05 21:29:46 -0500

    Change of variable

--- routes/admin.js
+++ routes/admin.js
@@ -12,7 +12,7 @@ router.route("/admin").get(async (req, res) => {
 
 		return res.status(200).render("saleshomepage", { salesInquiryList: inquiryList, title: "Sales Dashboard" });
 		//   return res.status(200).render('saleshomepage', {title: "Sales Dashboard", isLoggedIn : isLoggedIn, haserror:haserror});
-	} catch (e) {
+	} catch (error) {
 		haserror = true;
 		return res.status(400).render('error', {error:error});
 		//return res.status(400).render("saleshomepage", { salesInquiryList: inquiryList, title: "Sales Dashboard"});
