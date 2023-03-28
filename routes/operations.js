76224cd3f064f0f89c1df4bb0adf4d0c4d952565
Divyang <divyang@git.com>
2023-03-28 15:30:18 -0400

    added check for logging into operations dashboard

--- routes/operations.js
+++ routes/operations.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-  try {
-    return res.status(200).render("operationsDashboard", {
-      title: "Operations Dashboard",
-    });
-  } catch (error) {
-    return res.status(400).render("error", {error: error});
-  }
+	if (!req.session.user || req.session.user.role !== "operational manager") {
+		return res.redirect("/");
+	}
+	try {
+		return res.status(200).render("operationsDashboard", {
+			title: "Operations Dashboard",
+		});
+	} catch (error) {
+		return res.status(400).render("error", { error: error });
+	}
 });
 
 module.exports = router;
