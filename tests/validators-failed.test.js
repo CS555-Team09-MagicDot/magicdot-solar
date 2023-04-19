ca036a9e1ef7f6a68160b6d45db836c186ff16d2
Divyang <8656786+perseus01@users.noreply.github.com>
2023-04-19 19:09:58 -0400

    added failed test cases file for jest

---
+++ tests/validators-failed.test.js
@@ -0,0 +1,42 @@
+const validators = require("../validators");
+const ObjectId = require("mongodb").ObjectId;
+
+test("Should throw for invalid string", () => {
+	expect(() => validators.validateString(4545, "A field")).toThrow();
+});
+
+test("Should throw for invalid object ID", () => {
+	expect(() => validators.validateId("adkjfhgbj879809", "Test ID")).toThrow();
+});
+
+test("Should throw for invalid first name", () => {
+	expect(() => validators.validateName("John^&*", "first name")).toThrow();
+});
+
+test("Should throw for invalid last name", () => {
+	expect(() => validators.validateName("oujhy80", "last name")).toThrow();
+});
+
+test("Should throw for invalid role name", () => {
+	expect(() => validators.validateRole("Project Manager")).toThrow();
+});
+
+test("Should throw for invalid email", () => {
+	expect(() => validators.validateEmail("B4zvIxample.com")).toThrow();
+});
+
+test("Should throw for invalid phone number", () => {
+	expect(() => validators.validatePhone("Hi John")).toThrow();
+});
+
+test("Should throw for invalid Subject", () => {
+	expect(() => validators.validateSubject()).toThrow();
+});
+
+test("Should throw for invalid Message", () => {
+	expect(() => validators.validateMessage(8789809)).toThrow();
+});
+
+test("Should throw for invalid password", () => {
+	expect(() => validators.validatePassword("test@123")).toThrow();
+});
